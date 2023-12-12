1abb90dcfc1b16af268c0d9d4d87f609008f745d
jball434 <72677584+jball434@users.noreply.github.com>
2023-12-11 22:34:21 -0700

    Assignment 3

---
+++ index.js
@@ -0,0 +1,196 @@
+// import statements
+const fs = require('fs').promises;
+const express = require('express');
+const app = express();
+
+// Load JSON files
+const ARTISTS = './data/artists.json';
+const PAINTINGS = './data/paintings-nested.json';
+const GALLERIES = './data/galleries.json';
+
+// Variables to store the JSON data
+let artistsData; 
+let paintingsData; 
+let galleriesData; 
+
+// Read Artists Data
+async function readArtistsData() {
+  try {
+    const data = await fs.readFile(ARTISTS);
+    artistsData = JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data:', error);
+  }
+}
+
+// Read Paintings Data
+async function readPaintingsData() {
+  try {
+    const data = await fs.readFile(PAINTINGS);
+    paintingsData = JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data:', error);
+  }
+}
+
+// Read Galleries Data
+async function readGalleriesData() {
+  try {
+    const data = await fs.readFile(GALLERIES);
+    galleriesData = JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data:', error);
+  }
+}
+
+
+// Call the respective functions to load the data
+readArtistsData();
+readPaintingsData();
+readGalleriesData();
+
+// Middleware to parse JSON in incoming requests
+app.use(express.json());
+
+// Get all paintings
+app.get('/paintings', (req, res) => {
+  if(JSON.stringify(paintingsData).length > 0) {
+  res.json(paintingsData);
+  } else {
+    return res.status(404).send('Data not found on the server.');
+  }
+});
+
+// Get a specific painting
+app.get('/painting/:id', (req, res) => {
+  const id = req.params.id;
+  const painting = paintingsData.find((painting) => painting.paintingID == id);
+
+  if (!painting) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(painting);
+});
+
+// Get a specific gallery
+app.get('/painting/gallery/:id', (req, res) => {
+  const id = req.params.id;
+  const painting = paintingsData.find((painting) => {
+    return painting.gallery.galleryID == id;
+  });
+
+  if (!painting) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(painting);
+});
+
+// Get a specific painting matching the artist
+app.get('/painting/artist/:id', (req, res) => {
+  const id = req.params.id;
+  const artist = paintingsData.find((painting) => {
+    return painting.artist.artistID == id;
+  });
+
+  if (!artist) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(artist);
+});
+
+// Get the painting within the date range
+app.get('/painting/year/:min/:max', (req, res) => {
+  const min = req.params.min;
+  const max = req.params.max;
+
+  const paintings = paintingsData.filter((painting) => {
+    return painting.yearOfWork >= min && painting.yearOfWork <= max;
+  });
+
+  if (!paintings || paintings.length <= 0) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(paintings);
+});
+
+// Get the painting with title containing specific text
+app.get('/painting/title/:text', (req, res) => {
+  const text = req.params.text;
+  const paintings = paintingsData.filter((painting) => {
+    return (painting.title).toLowerCase().includes(text.toLowerCase());
+  });
+
+  if (!paintings || paintings.length <= 0) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(paintings);
+});
+
+// Get the painting with a specific color name
+app.get('/painting/color/:name', (req, res) => {
+  const name = req.params.name;
+  const paintings = paintingsData.filter((painting) => {
+    let arrayColor = painting.details.annotation.dominantColors;
+    return arrayColor.some(element => element.web.toLowerCase() == name.toLowerCase() || element.name.toLowerCase() == name.toLowerCase());
+  });
+
+  if (!paintings || paintings.length <= 0) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(paintings);
+});
+
+
+// Get all artists
+app.get('/artists', (req, res) => {
+  res.json(artistsData);
+});
+
+// Get a specific artist
+app.get('/artists/:country', (req, res) => {
+  const country = req.params.country;
+  const artists = artistsData.filter((artist) => {
+    return artist.Nationality.toLowerCase() == country.toLowerCase();
+  });
+
+  if (!artists || artists.length <= 0) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(artists);
+});
+
+// Get all the galleries
+app.get('/galleries', (req, res) => {
+  if(JSON.stringify(galleriesData).length > 0) {
+    res.json(galleriesData);
+  } else {
+    res.status(404).send('Data not found on the server.');
+  }
+});
+
+// Get gallery by country
+app.get('/galleries/:country', (req, res) => {
+  const country = req.params.country;
+  const galleries = galleriesData.filter((gal) => {
+    return gal.gallery.country.toLowerCase() == country.toLowerCase();
+  });
+
+  if (!galleries || galleries.length <= 0) {
+    res.status(404).send('Data not found on the server.');
+  }
+
+  res.json(galleries);
+});
+
+// Start the Express server
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
